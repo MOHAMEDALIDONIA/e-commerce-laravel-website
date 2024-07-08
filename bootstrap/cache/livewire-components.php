b8a0edcfc7004155be5839f720ff982fb1e535ea
MOHAMEDSDFCFCD <100955086+MOHAMEDSDFCFCD@users.noreply.github.com>
2024-07-08 19:13:58 +0300

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,11 @@
+<?php return array (
+  'admin.brands.index' => 'App\\Http\\Livewire\\Admin\\Brands\\Index',
+  'admin.category.index' => 'App\\Http\\Livewire\\Admin\\Category\\Index',
+  'frontend.cart.cart-count' => 'App\\Http\\Livewire\\Frontend\\Cart\\CartCount',
+  'frontend.cart.cart-show' => 'App\\Http\\Livewire\\Frontend\\Cart\\CartShow',
+  'frontend.checkout.checkout-show' => 'App\\Http\\Livewire\\Frontend\\Checkout\\CheckoutShow',
+  'frontend.product.index' => 'App\\Http\\Livewire\\Frontend\\Product\\Index',
+  'frontend.product.view' => 'App\\Http\\Livewire\\Frontend\\Product\\View',
+  'frontend.wishlist-count' => 'App\\Http\\Livewire\\Frontend\\WishlistCount',
+  'frontend.wishlist-show' => 'App\\Http\\Livewire\\Frontend\\WishlistShow',
+);
